0e2403684b150583188fb0cf38e94878d56dbd1b
Brando Rodriguez <brandorodrigu3z@gmail.com>
2023-06-17 00:52:01 -0500

    *

--- src/components/ui/footer/Footer.jsx
+++ src/components/ui/footer/Footer.jsx
@@ -1,12 +1,27 @@
 import styles from "./Footer.module.css";
-import { Logo, Youtube, Facebook, Whatsapp, Figma, Dribble, Phone, Apple, PlayStore, ArrowRight, Email, Mail, Address, Fax, Tel } from "../../ui";
+import {
+  Logo,
+  Youtube,
+  Facebook,
+  Whatsapp,
+  Figma,
+  Dribble,
+  Apple,
+  PlayStore,
+  ArrowRight,
+  Email,
+  Mail,
+  Address,
+  Fax,
+  Tel,
+  Shape,
+} from "../../ui";
 import { useLanguage } from "@context/LanguageContext/useLanguage";
 import { FooterText } from "@mocks/General";
 
 export const Footer = () => {
-
-  const { language } = useLanguage()
-  const content = FooterText[language]
+  const { language } = useLanguage();
+  const content = FooterText[language];
 
   const services_styles = [
     styles.wellness,
@@ -15,15 +30,18 @@ export const Footer = () => {
     styles.dental,
     styles.include,
     styles.test,
-    styles.therapy
-  ]
+    styles.therapy,
+  ];
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.content}>
+      <Shape />
+      <div className={`${styles.content} container`}>
         <div className={styles.detail}>
           <div className={styles.introduction}>
-            <Logo />
+            <div>
+              <Logo />
+            </div>
             <p>{content.description}</p>
             <label>{content.social_title}</label>
             <span className={styles.socials}>
@@ -35,27 +53,28 @@ export const Footer = () => {
             </span>
           </div>
           <div className={styles.category}>
-
-            {
-              content.services.map((service, index) => {
-                return (
-                  <span
-                    key={index}
-                    className={services_styles[index]}>
-                    <p>{service}</p>
-                    <ArrowRight />
-                  </span>
-                )
-              })
-            }
-
+            {content.services.map((service, index) => {
+              return (
+                <span key={index} className={services_styles[index]}>
+                  <p>{service}</p>
+                  <ArrowRight />
+                </span>
+              );
+            })}
           </div>
           <div className={styles.app}>
             <p>{content.app_section.title}</p>
             <label>{content.app_section.description}</label>
-            <Apple />
-            <PlayStore />
+            <div className={styles.group}>
+              <div>
+                <Apple />
+              </div>
+              <div>
+                <PlayStore />
+              </div>
+            </div>
           </div>
+          <img src="./images/footer/app.png" alt="" />
         </div>
         <div className={styles.newsletter}>
           <span className={styles.newsletterText}>
@@ -64,17 +83,21 @@ export const Footer = () => {
           </span>
           <form action="" className={styles.newsletterForm}>
             <Email />
-            <input type="text" className={styles.newsletterInput} placeholder={content.newsletter.input_placeholder} required />
-            <button type="button" className={styles.newsletterButton}>{content.newsletter.button_text}</button>
+            <input
+              type="text"
+              className={styles.newsletterInput}
+              placeholder={content.newsletter.input_placeholder}
+              required
+            />
+            <button type="button" className={styles.newsletterButton}>
+              {content.newsletter.button_text}
+            </button>
           </form>
         </div>
         <nav className={styles.menu}>
-          {
-            content.menu.map((item, index)=> {
-              return <p key={index}>{item}</p>
-            })
-          }
-          
+          {content.menu.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })}
         </nav>
         <div className={styles.bottombar}>
           <div>
