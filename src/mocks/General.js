314302f2d50831baf8154cb8c046290fe6b4b6bf
Enrique Trigoso <enrique.trigoso@evaseguros.es>
2023-06-16 22:36:55 -0500

    fix: Footer Ingles

--- src/mocks/General.js
+++ src/mocks/General.js
@@ -11,3 +11,65 @@ export const NavBarText = {
 		register_button: 'Register'
 	}
 }
+
+export const FooterText = {
+	es: {
+		description: 'Protejamos lo más importante para alcanzar el bienestar en nuestras vidas: tu salud y la de los tuyos.',
+		social_title: 'Síganos en nuestras redes:',
+		services: [
+			'Bienestar y Estética', 'Cirugías', 'Consultas médicas', 'Dental', 'Incluido en Eva Salud', 'Pruebas médicas', 'Tratamientos y terapias'
+		],
+		app_section: {
+			title: 'Descargar aplicación',
+			description: 'Descarga nuestras Apps y obtén 15% de descuento.'
+		},
+		newsletter: {
+			title: 'Suscríbete a nuestra newsletter',
+			description: 'Sé el primero en enterarte de descuentos, ofertas y eventos semanales en tu buzón.',
+			input_placeholder: 'Introduce tu correo electrónico',
+			button_text: 'Suscríbete'
+		},
+		menu: [
+			'Sobre nosotros',
+			'Especialidades',
+			'Blog',
+			'Prensa',
+			'Preguntas frecuentes',
+			'Trabaja con nosotros',
+			'Contáctanos',
+			'Política de privacidad',
+			'Términos y Condiciones',
+		],
+		copyright: '©Evasalud.com 2023. Todos los derechos reservados.'
+	},
+	en: {
+		description: 'Let us protect the most important thing to achieve well-being in our lives: your health and that of your loved ones.',
+		social_title: 'Follow us on our networks:',
+		services: [
+			'Wellness and Aesthetics', 'Surgeries', 'Medical Consultations', 'Dental', 'Included in Eva Health', 'Medical Tests', 'Treatments and Therapies'
+		],
+		app_section: {
+			title: 'Download application',
+			description: 'Download our Apps and get a 15% discount.'
+		},
+		newsletter: {
+			title: 'Subscribe to our newsletter',
+			description: 'Be the first to hear about discounts, offers and weekly events in your mailbox.',
+			input_placeholder: 'Enter your email',
+			button_text: 'Subscribe'
+		},
+		menu: [
+			'About us',
+			'Specialties',
+			'Blog',
+			'Press',
+			'Frequently asked questions',
+			'Work with us',
+			'Contact us',
+			'Privacy policy',
+			'Terms and Conditions',
+		],
+		copyright: '©Evahealth.com 2023. All rights reserved.'
+	}
+
+}
