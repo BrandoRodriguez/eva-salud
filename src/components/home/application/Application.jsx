60c1f06036c653eb9dfcceaa1c05575ead1a1745
Brando Rodriguez <brandorodrigu3z@gmail.com>
2023-06-19 20:48:37 -0500

    *

--- src/components/home/application/Application.jsx
+++ src/components/home/application/Application.jsx
@@ -1,71 +1,67 @@
-
 import styles from "./Application.module.css";
 import { ButtonArrow, DoctorHome, MedicalCheck, VideoCall } from "../../ui";
 import { useLanguage } from "@context/LanguageContext/useLanguage";
 import { AplicationText } from "@mocks/Pages/Home";
 import { processBoldText } from "@utils/functions";
 export const Application = () => {
+  const { language } = useLanguage();
+  const content = AplicationText[language];
 
-    const { language } = useLanguage()
-    const content = AplicationText[language]
-
-    return (
-        <>
-            <div className={`${styles.Appsection} container`}>
-                <div className={styles.appImage}>
-                <div className={styles.scanQR}>
+  return (
+    <>
+      <div className={`${styles.Appsection} container`}>
+        <div className={styles.appImage}>
+          {/* <div className={styles.scanQR}>
                     <img src="./images/application/logoBlue.png" className={styles.logo}/>
                     <p>{content.qr_title}</p>
                     <img src="./images/application/qr.png" className={styles.qr}/>
-                </div>
-                </div>
-                <div className={styles.appDescription}>
+                </div> */}
+        </div>
+        <div className={styles.appDescription}>
+          <h2>
+            {processBoldText(content.title).map(({ type, content }, index) => {
+              switch (type) {
+                case "text":
+                  return content + " ";
+                case "span":
+                  return <b key={index}>{content} </b>;
+              }
+            })}
+          </h2>
 
-                    <h2>
-
-                        {
-                            processBoldText(content.title).map(({ type, content }, index) => {
-
-                                switch (type) {
-                                    case 'text':
-                                        return content + ' '
-                                    case 'span':
-                                        return <b key={index}>{content} </b>
-                                }
-                            })
-                        }
-
-                    </h2>
-
-
-                    <div className={styles.featureApp}>
-                        <VideoCall />
-                        <span>
-                            <p>{content.feature[0].title}</p>
-                            <p className={styles.textFeature}>{content.feature[0].description}</p>
-                            <ButtonArrow name={content.feature[0].button_text} />
-                        </span>
-                    </div>
-                    <div className={styles.featureApp}>
-                        <MedicalCheck />
-                        <span>
-                            <p>{content.feature[1].title}</p>
-                            <p className={styles.textFeature}>{content.feature[1].description}</p>
-                            <ButtonArrow name={content.feature[1].button_text} />
-                        </span>
-                    </div>
-                    <div className={styles.featureApp}>
-                        <DoctorHome />
-                        <span>
-                            <p>{content.feature[2].title}</p>
-                            <p className={styles.textFeature}>{content.feature[2].description}</p>
-                            <ButtonArrow name={content.feature[2].button_text} />
-                        </span>
-                    </div>
-                        <button className={styles.openApp}>Abrir la App</button>
-
-                </div>
-            </div>
-        </>
-    )
-}
+          <div className={styles.featureApp}>
+            <VideoCall />
+            <span>
+              <p>{content.feature[0].title}</p>
+              <p className={styles.textFeature}>
+                {content.feature[0].description}
+              </p>
+              <ButtonArrow name={content.feature[0].button_text} />
+            </span>
+          </div>
+          <div className={styles.featureApp}>
+            <MedicalCheck />
+            <span>
+              <p>{content.feature[1].title}</p>
+              <p className={styles.textFeature}>
+                {content.feature[1].description}
+              </p>
+              <ButtonArrow name={content.feature[1].button_text} />
+            </span>
+          </div>
+          <div className={styles.featureApp}>
+            <DoctorHome />
+            <span>
+              <p>{content.feature[2].title}</p>
+              <p className={styles.textFeature}>
+                {content.feature[2].description}
+              </p>
+              <ButtonArrow name={content.feature[2].button_text} />
+            </span>
+          </div>
+          <button className={styles.openApp}>Abrir la App</button>
+        </div>
+      </div>
+    </>
+  );
+};
