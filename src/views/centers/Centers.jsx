8e06f3169db51f101e4c1c8437ab8f4babcfadea
Brando Rodriguez <brandorodrigu3z@gmail.com>
2023-06-23 11:34:57 -0500

    *

--- src/views/centers/Centers.jsx
+++ src/views/centers/Centers.jsx
@@ -1,5 +1,6 @@
 import styles from "../consultation/Consultation.module.css";
-import { Layout } from "../../layouts/Layout";
+// import styles from "./Centers.module.css";
+import { Layout2 } from "../../layouts/Layout";
 import Button from "@UI/ButtonDecesos/Button";
 import Work from "@components/icons/Work";
 import AngleDown from "@components/icons/AngleDown";
@@ -14,28 +15,26 @@ import Blog from "@UI/Blog/Blog";
 import Subscribe from "@UI/Subscribe/Subscribe";
 import FilterSection from "@components/pages/consultation/FilterSection/FilterSection";
 import { CardsText, FilterSectionsText } from "@mocks/Pages/Centers";
-import brand1 from '@assets/brands/brand1.png'
+import brand1 from "@assets/brands/brand1.png";
 import Professional from "@components/icons/Professional";
 import Card1 from "@UI/Card1/Card1";
 
 const Centers = () => {
-
   // const { language } = useLanguage();
   // const content = HeaderSectionText[language];
 
-  const FilterSections = FilterSectionsText['es']
-  const cards = CardsText['es']
+  const FilterSections = FilterSectionsText["es"];
+  const cards = CardsText["es"];
 
-  const total_pages = 8
-  const current_page = 6
+  const total_pages = 8;
+  const current_page = 6;
 
   // const isDesktop = useMediaQuery({ minWidth: 1024 })
-  const showFilter = useMediaQuery({ minWidth: 900 })
+  const showFilter = useMediaQuery({ minWidth: 900 });
 
   return (
-    <Layout>
-      <main className="container">
-
+    <Layout2>
+      <main className={`${styles.centers} container`}>
         <Filter
           items={[
             <Fragment key={1}>
@@ -53,13 +52,12 @@ const Centers = () => {
             <Fragment key={3}>
               <KeyNumbers />
               Busca por servicio...
-            </Fragment>
+            </Fragment>,
           ]}
         />
 
         <div className={styles.filter_head}>
           <div className={styles.left}>
-
             <h2>Filtros Avanzado</h2>
             <h3>Reset</h3>
             <h3>Visualizando 0-12 de 80 Consultas Médicas </h3>
@@ -67,62 +65,40 @@ const Centers = () => {
 
           <div className={styles.selects_container}>
             <div className={styles.select2}>
-
               <p>Cantidad:</p>
               <strong>12</strong>
               <AngleDown />
-
             </div>
 
             <div className={styles.select2}>
-
               <p>Ordenar por:</p>
               <strong>Centros incluidos primero</strong>
               <AngleDown />
-
             </div>
 
-            <div className={styles.filter_head_buttons}>
-
-            </div>
+            <div className={styles.filter_head_buttons}></div>
           </div>
         </div>
 
         <div className={styles.content}>
-
-          {
-            showFilter && (
-              <div className={styles.filter}>
-                {
-                  FilterSections.map((section, index) => {
-                    return (
-                      <FilterSection
-                        key={index}
-                        title={section.title}
-                        items={section.items}
-                      />
-                    )
-                  })
-                }
-
-
-              </div>
-            )
-          }
+          {showFilter && (
+            <div className={styles.filter}>
+              {FilterSections.map((section, index) => {
+                return (
+                  <FilterSection
+                    key={index}
+                    title={section.title}
+                    items={section.items}
+                  />
+                );
+              })}
+            </div>
+          )}
           <div>
             <div className={styles.cards_container}>
-
-              {
-
-                cards.map((card, index) => {
-                  return (
-                    <Card1 key={index}>
-
-                    </Card1>
-                  )
-                })
-              }
-
+              {cards.map((card, index) => {
+                return <Card1 key={index}></Card1>;
+              })}
             </div>
 
             <div className={styles.pagination}>
@@ -131,14 +107,11 @@ const Centers = () => {
               </button>
 
               <div className={styles.pages}>
-                {
-                  Array(total_pages).fill().map((_, i) => {
-
-                    return (
-                      <span key={i}>{i + 1}</span>
-                    )
-                  })
-                }
+                {Array(total_pages)
+                  .fill()
+                  .map((_, i) => {
+                    return <span key={i}>{i + 1}</span>;
+                  })}
               </div>
 
               <button className={styles.circle_button}>
@@ -146,15 +119,13 @@ const Centers = () => {
               </button>
             </div>
           </div>
-
         </div>
 
         <Blog />
 
         <Subscribe />
-
       </main>
-    </Layout>
+    </Layout2>
   );
 };
 
